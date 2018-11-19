5cccd186966996bf13ae0c2c9a5ebc25f224da96
seergiioo6 <sergio.anru@gmail.com>
2018-11-19 20:58:34 +0100

    contact page

--- routes/index.js
+++ routes/index.js
@@ -34,10 +34,10 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
+	app.all('/contact', routes.views.contact);
 	app.get('/:category?', routes.views.index);
 	app.get('/:category?/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
